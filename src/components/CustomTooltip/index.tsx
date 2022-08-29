6e7a2cefb663b09f513c05c49c7fa38761397017
Sergey Vlasov <sidodji1337@yandex.ru>
2022-08-30 01:21:19 +0300

    fix

--- src/components/CustomTooltip/index.tsx
+++ src/components/CustomTooltip/index.tsx
@@ -1,11 +1,17 @@
 /* eslint-disable camelcase */
+import moment from 'moment';
+import 'moment/locale/ru';
 import React from 'react';
 import { TModifiedJsonData } from 'types';
+
+moment.locale('ru');
 
 export const CustomTooltip = ({ active, payload }) => {
     if (active && payload && payload.length) {
         const [data] = payload;
-        const { restaurant, worked_hours } = data.payload as TModifiedJsonData;
+        const {
+            restaurant, worked_hours, dt_start, dt_end,
+        } = data.payload as TModifiedJsonData;
 
         return (
             <div className="custom-tooltip">
@@ -14,6 +20,8 @@ export const CustomTooltip = ({ active, payload }) => {
                     ? <p className="desc">Плановая длительность смены: {worked_hours}ч.</p>
                     : <p className="desc">Отработал часов по факту: {worked_hours}ч.</p>
                 }
+                <p className="desc">C {moment(dt_start).format('LT')}</p>
+                <p className="desc">По {moment(dt_end).format('LT')}</p>
             </div>
         );
     }
